c5919485fafc6dbee5e2585cc46abaf3989ff970
Yamil Hallal Hernandez <yamilhallalhernandez@MacBook-Air-de-Yamil.local>
2022-04-25 20:02:09 -0500

    Yamil

---
+++ db/update.sql
@@ -0,0 +1,8 @@
+alter table contact add column adress varchar(30)
+alter table contact add columnuserid smallserial;
+alter table contact add columnlastname varchar(30);
+alter table contact add columnemail varchar(30);
+alter table contact add columncellphone varchar(30);
+alter table contact add columnphone varchar(30);
+alter table contact add columnbirthday date;
+alter table contact add columnfavorite boolean;
